1bd40fdc2a10f6a7882a1cbfb0b7b0ec8067647b
Manikanta Thummuri <thummurimanikanta7@gmail.com>
2026-03-31 18:45:40 +0530

    add primsa

---
+++ backend/test.ts
@@ -0,0 +1,13 @@
+import { prisma } from "./prisma/db";
+
+// create a new user
+await prisma.user.create({
+  data: {
+    name: "John Dough",
+    email: `john-${Math.random()}@example.com`,
+  },
+});
+
+// count the number of users
+const count = await prisma.user.count();
+console.log(`There are ${count} users in the database.`);
